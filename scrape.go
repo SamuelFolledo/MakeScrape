ef530fada7c7d28840f81968e2cb618711ca74dc
Samuel Folledo <samuelfolledo@gmail.com>
2020-02-06 16:21:05 -0800

    [add] initialize project

---
+++ scrape.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gocolly/colly"
+)
+
+// main() contains code adapted from example found in Colly's docs:
+// http://go-colly.org/docs/examples/basic/
+func main() {
+	// Instantiate default collector
+	c := colly.NewCollector()
+
+	// On every a element which has href attribute call callback
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		// Print link
+		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+	})
+
+	// Before making a request print "Visiting ..."
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
+
+	c.OnScraped(func(r *colly.Response) {
+		fmt.Println("Finished", r.Request.URL)
+	})
+
+	// Start scraping on https://hackerspaces.org
+	c.Visit("https://www.ebay.com/deals")
+}
