d970672813a35f76172c9dfa3806d4e3b745963d
Samuel Folledo <samuelfolledo@gmail.com>
2020-02-18 22:38:26 -0800

    [add] turned scrapeString to Deal

--- scrape.go
+++ scrape.go
@@ -2,15 +2,91 @@ package main
 
 import (
 	"fmt"
+	"strconv" //convert string to float
+	"strings"
 
 	"github.com/gocolly/colly"
+	"github.com/gocolly/colly/debug"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
+
+type Deal struct {
+	gorm.Model
+	Name          string  `json:"name" gorm:"name"`
+	CurrentPrice  float64 `json:"currentPrice" gorm:"currentPrice"`
+	PreviousPrice float64 `json:"previousPrice" gorm:"previousPrice"`
+}
 
 // main() contains code adapted from example found in Colly's docs:
 // http://go-colly.org/docs/examples/basic/
 func main() {
+	// testDB()
+	scrapeEbay()
+	db, err := gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	var deal Deal
+	db.First(&deal, 1)
+	println("DEAL 1 = ", deal.Name)
+}
+
+func testDB() {
+	db, err := gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	// Migrate the schema
+	db.AutoMigrate(&Deal{})
+
+	// Create
+	db.Create(
+		&Deal{Name: "Deal 1", CurrentPrice: 1000, PreviousPrice: 9999},
+	)
+
+	// Read
+	var deal Deal
+	db.First(&deal, 1)                    // find product with id 1
+	db.First(&deal, "name = ?", "Deal 1") // find product with code L1212
+
+	fmt.Println("Deal = ", deal.Name, " ", deal.CurrentPrice)
+
+	// Update - update product's price to 2000
+	db.Model(&deal).Update("Price", 2000)
+
+	fmt.Println("Deal = ", deal.Name, " ", deal.CurrentPrice)
+
+	// Delete - delete product
+	db.Delete(&deal)
+}
+
+func scrapeEbay() {
 	// Instantiate default collector
-	c := colly.NewCollector()
+	c := colly.NewCollector(
+		colly.Async(true),                    // Turn on asynchronous requests
+		colly.Debugger(&debug.LogDebugger{}), // Attach a debugger to the collector
+	)
+
+	db, err := gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	// Migrate the schema
+	db.AutoMigrate(&Deal{})
+
+	c.Limit(&colly.LimitRule{
+		DomainGlob:  "*httpbin.*", // when visiting links which domains' matches "*httpbin.*" glob
+		Parallelism: 2,            // Limit the number of threads started by colly to two
+		//Delay:      5 * time.Second,
+	})
 
 	//SELECTORS LIST
 	//Spotlight Deal = #refit-spf-container > div.sections-container > div.ebayui-dne-featured-card.ebayui-dne-featured-with-padding > div.ebayui-dne-summary-card.card.ebayui-dne-item-featured-card--topDeals.ebayui-dne-featured-with-carousel > div > div > div.dne-itemtile-detail
@@ -20,11 +96,19 @@ func main() {
 	//Featured Deals 2 (div:nth-child(1)): #refit-spf-container > div.sections-container > div.ebayui-dne-featured-card.ebayui-dne-featured-with-padding > div.ebayui-dne-item-featured-card.ebayui-dne-item-featured-card > div > div:nth-child(1) > div > div.dne-itemtile-detail
 
 	// On every a element which has href attribute call callback
-	c.OnHTML("#refit-spf-container > div.sections-container > div.ebayui-dne-featured-card.ebayui-dne-featured-with-padding > div.ebayui-dne-item-featured-card.ebayui-dne-item-featured-card > div", func(e *colly.HTMLElement) {
+	c.OnHTML("#refit-spf-container > div.sections-container > div.ebayui-dne-featured-card.ebayui-dne-featured-with-padding > div.ebayui-dne-item-featured-card.ebayui-dne-item-featured-card > div > div > div > div.dne-itemtile-detail", func(e *colly.HTMLElement) {
 		// link := e.Attr("href")
 		// Print link
-		print("\n++++++++E TEXT = ", e.Text, "\n\n")
-		fmt.Printf("\n++++++++Link found: -> %s\n\n", e.Attr("href"))
+		print("\n==========================================================")
+		// print("\n++++++++E TEXT = ", e.Text, "\n\n")
+		// print("\n++++++++E DOM = ", e.DOM.Text(), "\n\n") //e.DOM.Text() returns the same as e.Text
+		var dealsScraped = e.DOM.Text()
+		createDealFromScrapedText(dealsScraped)
+
+		// fmt.Printf("\n++++++++Link found: -> %s\n\n", e.Attr("href"))
+		// fmt.Println("PARAGRAPHS", e.DOM.Find("p").Text())
+		// var scrapeResult = e.DOM.Text()
+		print("----------------------------------------------------------\n")
 	})
 
 	// Before making a request print "Visiting ..."
@@ -38,4 +122,29 @@ func main() {
 
 	// Start scraping on https://hackerspaces.org
 	c.Visit("https://www.ebay.com/deals")
+
+	// for i := 0; i < 1; i++ { // Start scraping in five threads on https://httpbin.org/delay/2
+	// 	c.Visit(fmt.Sprintf("%s?n=%d", "https://www.ebay.com/deals", i))
+	// }
+
+	c.Wait() // Wait until threads are finished
+}
+
+func createDealFromScrapedText(dealsScraped string) {
+	dealArray := strings.Split(dealsScraped, "$")
+	// fmt.Println("deals Scraped = ", s)
+	var name = dealArray[0]
+	currentPrice, _ := strconv.ParseFloat(dealArray[1], 64)
+	var previousPrice float64
+	if len(dealArray) > 2 {
+		prev, _ := strconv.ParseFloat(dealArray[2], 64)
+		previousPrice = prev
+	}
+	// for index, word := range s {
+	// 	fmt.Println(index, word)
+	// }
+	print("DEAL IS ", name, " CURRENT PRICE = ", currentPrice, " PREVIOUSLY = ", previousPrice)
+	// db.Create(
+	// 	&Deal{Name: e.DOM.Text(), Price: 1000},
+	// )
 }
