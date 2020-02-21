4b4f696f29f1d53e72dd25ee48ee97510c014ff8
Samuel Folledo <samuelfolledo@gmail.com>
2020-02-20 22:04:44 -0800

    [ADD] scrape and ecolly works

--- scrape.go
+++ scrape.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"strconv" //convert string to float
 	"strings"
 
 	"github.com/gocolly/colly"
 	"github.com/gocolly/colly/debug"
+	"github.com/labstack/echo"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
@@ -22,13 +24,13 @@ type Deal struct {
 // main() contains code adapted from example found in Colly's docs:
 // http://go-colly.org/docs/examples/basic/
 func main() {
-	// testDB()
 	scrapeEbay()
-	db, err := gorm.Open("sqlite3", "test.db")
-	if err != nil {
-		panic("failed to connect database")
-	}
-	defer db.Close()
+
+	// db, err := gorm.Open("sqlite3", "test.db")
+	// if err != nil {
+	// 	panic("failed to connect database")
+	// }
+	// defer db.Close()
 
 	// var deal Deal
 	// db.First(&deal, 1)
@@ -69,45 +71,32 @@ func testDB() {
 func scrapeEbay() {
 	// Instantiate default collector
 	c := colly.NewCollector(
-		colly.Async(true),                    // Turn on asynchronous requests
+		colly.Async(false),                   // Turn on asynchronous requests, this stuff could return before or after.
 		colly.Debugger(&debug.LogDebugger{}), // Attach a debugger to the collector
 	)
 
-	db, err := gorm.Open("sqlite3", "test.db")
-	if err != nil {
-		panic("failed to connect database")
-	}
+	// db, err := gorm.Open("sqlite3", "test.db")
+	// if err != nil {
+	// 	panic("failed to connect database")
+	// }
 	// defer db.Close()
 	// Migrate the schema
-	db.AutoMigrate(&Deal{})
+	// db.AutoMigrate(&Deal{})
 
-	c.Limit(&colly.LimitRule{
-		DomainGlob:  "*httpbin.*", // when visiting links which domains' matches "*httpbin.*" glob
-		Parallelism: 2,            // Limit the number of threads started by colly to two
-		//Delay:      5 * time.Second,
-	})
+	// c.Limit(&colly.LimitRule{ //limit must be use to async = true. replace httpbin to ebay.com
+	// 	DomainGlob:  "*httpbin.*", // when visiting links which domains' matches "*httpbin.*" glob
+	// 	Parallelism: 2,            // Limit the number of threads started by colly to two
+	// 	//Delay:      5 * time.Second,
+	// })
 
-	//SELECTORS LIST
-	//Spotlight Deal = #refit-spf-container > div.sections-container > div.ebayui-dne-featured-card.ebayui-dne-featured-with-padding > div.ebayui-dne-summary-card.card.ebayui-dne-item-featured-card--topDeals.ebayui-dne-featured-with-carousel > div > div > div.dne-itemtile-detail
-	//Trending Deals = #refit-spf-container > div.sections-container > div.ebayui-dne-featured-card.ebayui-dne-featured-with-padding > div.ebayui-dne-carousel.filmstrip-centered.ebayui-dne-carousel.ebayui-dne-trending-widget.filmstrip-1 > div
-	//Trending Deals 2 (li:nth-child(9)) = #refit-spf-container > div.sections-container > div.ebayui-dne-featured-card.ebayui-dne-featured-with-padding > div.ebayui-dne-carousel.filmstrip-centered.ebayui-dne-carousel.ebayui-dne-trending-widget.filmstrip-1 > div > ul > li:nth-child(9) > div > div.dne-itemtile-detail
-	//Featured Deals: #refit-spf-container > div.sections-container > div.ebayui-dne-featured-card.ebayui-dne-featured-with-padding > div.ebayui-dne-item-featured-card.ebayui-dne-item-featured-card > div
-	//Featured Deals 2 (div:nth-child(1)): #refit-spf-container > div.sections-container > div.ebayui-dne-featured-card.ebayui-dne-featured-with-padding > div.ebayui-dne-item-featured-card.ebayui-dne-item-featured-card > div > div:nth-child(1) > div > div.dne-itemtile-detail
+	var deals []string
 
 	// On every a element which has href attribute call callback
 	c.OnHTML("#refit-spf-container > div.sections-container > div.ebayui-dne-featured-card.ebayui-dne-featured-with-padding > div.ebayui-dne-item-featured-card.ebayui-dne-item-featured-card > div > div > div > div.dne-itemtile-detail", func(e *colly.HTMLElement) {
-		// link := e.Attr("href")
-		// Print link
-		print("\n==========================================================")
-		// print("\n++++++++E TEXT = ", e.Text, "\n\n")
-		// print("\n++++++++E DOM = ", e.DOM.Text(), "\n\n") //e.DOM.Text() returns the same as e.Text
 		var dealsScraped = e.DOM.Text()
-		deal := createDealFromScrapedText(dealsScraped)
-		db.Create(&deal)
-		// fmt.Printf("\n++++++++Link found: -> %s\n\n", e.Attr("href"))
-		// fmt.Println("PARAGRAPHS", e.DOM.Find("p").Text())
-		// var scrapeResult = e.DOM.Text()
-		print("----------------------------------------------------------\n")
+		deals = append(deals, dealsScraped)
+		// deal := createDealFromScrapedText(dealsScraped)
+		// db.Create(&deal)
 	})
 
 	// Before making a request print "Visiting ..."
@@ -119,24 +108,24 @@ func scrapeEbay() {
 		fmt.Println("Finished", r.Request.URL)
 	})
 
-	// Start scraping on https://hackerspaces.org
+	e := echo.New() //create a server, inir xollt
+	e.GET("/scrape", func(ec echo.Context) (err error) {
+		c.Visit("https://www.ebay.com/deals")
+		c.Wait()
+
+		var firstDeal string
+		for _, item := range deals {
+			firstDeal += "- " + item + "\n"
+		}
+		// return ec.JSON()
+		return ec.String(http.StatusOK, firstDeal)
+	})
+
+	e.Logger.Fatal(e.Start(":1323"))
+
 	c.Visit("https://www.ebay.com/deals")
 
-	// for i := 0; i < 1; i++ { // Start scraping in five threads on https://httpbin.org/delay/2
-	// 	c.Visit(fmt.Sprintf("%s?n=%d", "https://www.ebay.com/deals", i))
-	// }
-
 	c.Wait() // Wait until threads are finished
-
-	deals := Deal{}
-	db.Find(&deals)
-
-	fmt.Println("\n\nDEALS ARE ===== ", deals.Name)
-	// for _, deal := range deals {
-
-	// }
-
-	defer db.Close()
 }
 
 func createDealFromScrapedText(dealsScraped string) Deal {
